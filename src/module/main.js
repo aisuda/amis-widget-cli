d7caceb85faf028f9d43575227af6c38d8334233
wibetter <365533093@qq.com>
2022-01-04 20:41:35 +0800

    docs: 发一版内测 1.0.9

--- src/module/main.js
+++ src/module/main.js
@@ -12,20 +12,7 @@ module.exports = {
   inspect,
   amisConfigInit,
   dev: () => {
-    const curDevConfig = curConfig;
-    /*// 设置editor入口
-    const devEditorPath = path.resolve(__dirname, '../editor/EditorDemo.jsx'); // 从akfun中获取
-    if (curDevConfig.dev && curDevConfig.dev.entry) {
-      Object.keys(curDevConfig.dev.entry).forEach((name) => {
-        curDevConfig.dev.entry[name] = [devEditorPath].concat(curDevConfig.dev.entry[name]);
-      });
-    }
-    // 将当前editor添加为工程有效目录
-    if (curDevConfig.webpack) {
-      curDevConfig.webpack.projectDir = curDevConfig.webpack.projectDir ? curDevConfig.webpack.projectDir : [];
-      curDevConfig.webpack.projectDir.push(path.resolve(__dirname, '../editor'));
-    }*/
-    akfun.dev(curDevConfig, consoleTag);
+    akfun.dev(curConfig, consoleTag);
   }, // 构建脚本：开发环境
   build: () => akfun.build('build', curConfig, consoleTag), // 构建脚本：生产环境
   build2lib: () => akfun.build('lib', curConfig, consoleTag), // 构建脚本：生产环境
